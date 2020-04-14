500d2517b420aef8b9016ca2529ba17db09cb76d
AlundraOz <ailuozols@gmail.com>
2020-04-14 10:35:36 +0200

    quest020

--- front/odissey-homer/src/SignUp.js
+++ front/odissey-homer/src/SignUp.js
@@ -1,24 +1,50 @@
 import React from 'react';
 
+const initialState = {
+    email: '',
+    password: '',
+    name: '',
+    lastname: ''
+}
+
+
 class SignUp extends React.Component {
 constructor(props) {
     super(props);
-    this.state= {
-        email: ''
-    }
-    this.updateEmailField = this.updateEmailField.bind(this);
+    this.state= initialState
 }
 
-updateEmailField(event) {
+updateEmailField = (event) => {
     this.setState({email: event.target.value})
 }
 
+updatePasswordField = (event) => {
+    this.setState({password: event.target.value})
+}
+
+updateNameField = (event) => {
+    this.setState({name: event.target.value})
+}
+
+updateLastnameField = (event) =>  {
+    this.setState({lastname: event.target.value})
+}
+
+handleSubmit = (event) => {
+    console.log(this.state)
+    event.preventDefault();
+}
   render() {
     return(
     <div> 
-        <h1>{this.state.email}</h1>
-        <input type="email" name="email" onChange={this.updateEmailField}/>
-
+        <h1>{JSON.stringify(this.state,1,1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+            <input type="email" name="email" onChange={this.updateEmailField} placeholder="email"/>
+            <input type='password' name='password' onChange={this.updatePasswordField}/>
+            <input type='text' name='name' onChange={this.updateNameField}/>
+            <input type='text' name='lastname' onChange={this.updateLastnameField}/>
+            <input type="submit" value="Submit"/>
+        </form>
     </div>
     );
   }
